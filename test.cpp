01556e680443a865f2518c7e07b13a57804802d5
lengjianzheng <157360765@qq.com>
2015-12-12 20:37:12 -0800

    test

---
+++ test.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 12 Dec 2015 08:32:41 PM PST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world!"<<endl;
+}
